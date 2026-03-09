e776478a419a15f6e5c5d334f6c07dc4f5f27df2
Gabriela Ramirez <gaby.rmz.mtz@hotmail.com>
2026-03-09 16:36:29 -0600

    Agregué tops, crop top y leggings flare. Falta agregar espaldas a tops

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
-function cambiarImagen(id, imagen){
-document.getElementById(id).src = imagen;
+function cambiarImagen(id, frente, espalda) {
+
+    const imgFrente = document.getElementById(id);
+    const producto = imgFrente.closest(".producto");
+    const imgEspalda = producto.querySelector(".img-espalda");
+
+    imgFrente.src = frente;
+
+    if (imgEspalda && espalda) {
+        imgEspalda.src = espalda;
+    }
+
 }
+
+let startX = 0;
+let endX = 0;
+
+document.querySelectorAll(".producto-img").forEach(producto => {
+
+    producto.addEventListener("touchstart", function (e) {
+        startX = e.touches[0].clientX;
+    });
+
+    producto.addEventListener("touchend", function (e) {
+
+        endX = e.changedTouches[0].clientX;
+
+        const frente = this.querySelector(".img-frente");
+        const espalda = this.querySelector(".img-espalda");
+
+        if (!espalda) return;
+
+        if (startX - endX > 50) {
+            espalda.style.opacity = "1";
+            frente.style.opacity = "0";
+        }
+
+        if (endX - startX > 50) {
+            espalda.style.opacity = "0";
+            frente.style.opacity = "1";
+        }
+
+    });
+
+});
