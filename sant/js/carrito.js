bdefa458baebcd1b00bc0acd8917f32788bcc096
GabyRam <gaby.rmz.mtz@hotmail.com>
2026-09-04 16:38:40 -0600

    Ropa nueva en inicio

--- sant/js/carrito.js
+++ sant/js/carrito.js
@@ -16,7 +16,7 @@ const PRECIOS = {
     'Biker': 389, 'Leggings': 499, 'Legging Yoga': 499,
     'Pantalón Yoga': 499, 'Chamarra': 499, 'Jumper': 499,
     'Falda': 499, 'Legging Flare': 549, 'Calceta Yoga': 65,
-    'Calceta Moda': 60, 'Vestido': 549
+    'Calceta Moda': 60, 'Vestido': 549, 'Flow Pants': 549
 };
 
 const CALCETAS_PRECIOS = {
